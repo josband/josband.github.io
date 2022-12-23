a797bdc4f55a893f2902979dd91e4741ec3d482d
Jonah Osband <79324007+josband@users.noreply.github.com>
2022-12-23 16:40:22 -0500

    Body structure set up

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,22 @@ import "./Home.css";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="profile">
+      <article className="summary">
+        <p className="typing">Hello, I'm Jonah Osband</p>
+        <p>Computer Science student at the University of Pittsburgh</p>
+        <p>
+          I am currently a junior working to complete my bachelors degree, with
+          an interest in full-stack development and machine learning
+        </p>
+      </article>
+
+      {/* 
+        Need to place the image within the div to make adding the border not 
+        show the edge
+      */}
+      <div className="user-image"></div>
+    </section>
   );
 };
 
