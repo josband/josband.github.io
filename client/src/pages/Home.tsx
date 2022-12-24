a779061838a7362a0432001a193a1dada16f82e4
Jonah Osband <79324007+josband@users.noreply.github.com>
2022-12-23 21:36:37 -0500

    Fixed Footer

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,22 +2,23 @@ import "./Home.css";
 
 const Home: React.FC = () => {
   return (
-    <section className="profile">
-      <article className="summary">
-        <p className="typing">Hello, I'm Jonah Osband</p>
-        <p>Computer Science student at the University of Pittsburgh</p>
-        <p>
-          I am currently a junior working to complete my bachelors degree, with
-          an interest in full-stack development and machine learning
-        </p>
-      </article>
-
-      {/* 
+    <>
+      <section className="profile">
+        <article className="summary">
+          <p className="typing">Hello, I'm Jonah Osband</p>
+          <p>Computer Science student at the University of Pittsburgh</p>
+          <p>
+            I am currently a junior working to complete my bachelors degree,
+            with an interest in full-stack development and machine learning
+          </p>
+        </article>
+        {/* 
         Need to place the image within the div to make adding the border not 
         show the edge
       */}
-      <div className="user-image"></div>
-    </section>
+        <div className="user-image"></div>
+      </section>
+    </>
   );
 };
 
